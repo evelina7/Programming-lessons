69681eed7534412b2de2d0eca44ed2018d86d64b
Evelina D <evelinadiksaite@gmail.com>
2023-10-18 13:10:12 +0300

    Homework - added task 03

---
+++ Task_03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new string('*', 10));
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine("*        *");
+            Console.WriteLine(new string('*', 10));
+        }
+    }
+}
