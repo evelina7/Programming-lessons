5565857ed45876f93d2cd99a572ad2192ee6dec3
Evelina D <evelinadiksaite@gmail.com>
2023-10-18 13:23:46 +0300

    Homework - added task 02 (full)

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -13,13 +13,22 @@ namespace Task_02
             string name = "Evelina";
 
             Console.WriteLine(new string('-', 30));
-            Console.WriteLine("|                            |");
-            Console.Write("|        ");
-            Console.Write("Mano vardas         |\n|           {0}", name);
-            Console.WriteLine("          |        ");
-            Console.WriteLine("|                            |");
-
-
+            Console.Write("|");
+            Console.Write(new string(' ', 28));
+            Console.WriteLine("|");
+            Console.Write("|");
+            Console.Write(new string(' ', 9));
+            Console.Write("Mano vardas");
+            Console.Write(new string(' ', 8));
+            Console.WriteLine("|");
+            Console.Write("|");
+            Console.Write(new string(' ', 11));
+            Console.Write("{0}", name);
+            Console.Write(new string(' ', 10));
+            Console.WriteLine("|");
+            Console.Write("|");
+            Console.Write(new string(' ', 28));
+            Console.WriteLine("|");
             Console.WriteLine(new string('-', 30));
         }
     }
