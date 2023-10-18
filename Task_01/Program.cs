4b35d911157bd4db842cfbb5498456d11152b372
Evelina D <evelinadiksaite@gmail.com>
2023-10-18 12:09:49 +0300

    Homework - added task 01

---
+++ Task_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int a = 20;
+            Console.WriteLine("Įvestas amžius: {0}", a);
+            
+        }
+    }
+}
