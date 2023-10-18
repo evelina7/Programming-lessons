6325e922061af0e39c830272606ac517e7f68ea6
Evelina D <evelinadiksaite@gmail.com>
2023-10-18 13:28:36 +0300

    Homework - added task 04

---
+++ Task_04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var marke = "Audi";
+            var modelis = "Q3";
+            var metai = 2022;
+            var rida = 10000;
+
+            Console.WriteLine("Automobilio {0} {1} ({2} m.) rida - {3} km." , marke,modelis,metai,rida);
+
+        }
+    }
+}
