d5bcad9128fe97ff736c744cc452f8828742a403
Evelina D <evelinadiksaite@gmail.com>
2023-10-18 13:06:00 +0300

    Homework - added task 02

---
+++ Task_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Evelina";
+
+            Console.WriteLine(new string('-', 30));
+            Console.WriteLine("|                            |");
+            Console.Write("|        ");
+            Console.Write("Mano vardas         |\n|           {0}", name);
+            Console.WriteLine("          |        ");
+            Console.WriteLine("|                            |");
+
+
+            Console.WriteLine(new string('-', 30));
+        }
+    }
+}
