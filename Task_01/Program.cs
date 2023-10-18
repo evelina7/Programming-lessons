e90c6d1167c077237de73639a6045ac003647702
Evelina D <evelinadiksaite@gmail.com>
2023-10-18 12:49:13 +0300

    Homework - added task 01 (full)

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Task_01
 {
     internal class Program
@@ -12,9 +13,49 @@ namespace Task_01
         {
             Console.OutputEncoding = Encoding.UTF8;
 
+            /*
+             Parašykite programą, kuri išvestų žodį “Labas” ir 
+             jūsų vardą toje pačioje eilutėje.
+             */
+
+            Console.Write("1. ");
+            Console.Write("Labas, Evelina");
+            Console.WriteLine("");
+
+            /*
+             Susikurkite kintamąjį žmogaus amžiui nusakyti, išveskite 
+            į ekraną tekstą su šiuo amžiumi: “Įvestas amžius: {amžius}”. 
+            Tarp {} nurodytas kintamasis, kurio reikšmė toje vietoje 
+            turi būti įstatyta.
+             */
+      
             int a = 20;
-            Console.WriteLine("Įvestas amžius: {0}", a);
-            
+            Console.Write("2. ");
+            Console.Write("Įvestas amžius: {0}", a);
+            Console.WriteLine("");
+
+            /*
+             Susikurkite skaičiaus kintamąjį, kurį išveskite 
+            penkis kartus toje pačioje eilutėje be tarpų tarp 
+            šių skaičių (skaičius - 25, išvedimas - 2525252525).
+             */
+
+            var number = 25;
+
+            Console.Write("3. ");
+            Console.Write("{0}{1}{2}{3}{4}",number,number,number,number,number);
+            Console.WriteLine("");
+
+            /*
+             Susikurkite skaičiaus kintamąjį, kurį išveskite penkis 
+            kartus toje pačioje eilutėje su tarpais tarp šių skaičių 
+            (skaičius - 25, rezultatas - 25 25 25 25 25).
+             */
+
+            Console.Write("4. ");
+            Console.Write("{0} {1} {2} {3} {4}", number, number, number, number, number);
+            Console.WriteLine("");
+
         }
     }
 }
