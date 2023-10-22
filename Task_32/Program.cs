dc207408dc6170a918a9dd567ae884352253a4bc
Evelina D <evelinadiksaite@gmail.com>
2023-10-22 19:02:19 +0300

    Homework - added task 32

---
+++ Task_32/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //Paprašyti įvesti vartotojo vardą.
+            //Sukurti string tipo metodą, kuris gražina frazę pvz „Sveiki, Mantas“.
+            //Išvesti šią frazę į konsolę.
+
+            Console.Write("Įveskite savo vardą: ");
+            string ivestasVardas = Console.ReadLine();
+            Program p = new Program();
+            var result = p.Vardas(ivestasVardas);
+            Console.WriteLine($"Sveiki, {result}!");
+            Console.WriteLine();
+        }
+        private string Vardas(string vardas)
+        {
+            return vardas;
+        }
+    }
+}
