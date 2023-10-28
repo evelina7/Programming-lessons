1a67848314aaca5f99722c5c94a11c0b88b892be
Evelina D <evelinadiksaite@gmail.com>
2023-10-28 16:33:22 +0300

    Task 31 - fixed by comments

--- Task_31/Program.cs
+++ Task_31/Program.cs
@@ -14,25 +14,24 @@ namespace Task_31
 
             //Parašykite sumos funkciją su 3 parametrais, kuris gražina int tipo atsakymą.
             Console.WriteLine("Užduotis 1. ");
-            Program p1 = new Program();
-            var result1 = p1.Suma1(7,12,80);
+            int result1 = Suma1(7,12,80);
             Console.WriteLine(result1);
             Console.WriteLine();
 
             //Parašykite dalmens funkciją su 2 parametrais, kuris gražina double tipo  atsakymą.
             Console.WriteLine("Užduotis 2. ");
-            Program p2 = new Program();
-            var result2 = p2.Suma2(80,10);
+            double result2 = Dalmuo(80,10);
             Console.WriteLine(result2);
             Console.WriteLine();
 
         }
-        private int Suma1(int a, int b, int c)
+
+        static int Suma1(int a, int b, int c)
         {
             return a + b + c; 
         }
 
-        private double Suma2(double a, double b)
+        static double Dalmuo(double a, double b)
         {
             return a / b;
         }
